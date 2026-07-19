408d5f72a28dfd445c8f45418f94d6515ad256ff
yuanweijie <yuanweijie@example.com>
2026-07-19 10:58:39 +0800

    test: add Unity runtime and edit mode assemblies

---
+++ EndangeredAR/Assets/Scripts/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("EndangeredAR.Tests.EditMode")]
+[assembly: InternalsVisibleTo("EndangeredAR.Tests.PlayMode")]
---
+++ EndangeredAR/Assets/Tests/EditMode/ExistingDemoSmokeTests.cs
@@ -0,0 +1,27 @@
+using EndangeredAR.Missions;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace EndangeredAR.Tests.EditMode
+{
+    public class ExistingDemoSmokeTests
+    {
+        [Test]
+        public void SensenFoodMission_AwardsPointsOnlyOnce()
+        {
+            var go = new GameObject("Mission Test");
+            try
+            {
+                var controller = go.AddComponent<MissionController>();
+                controller.StartFoodMission();
+                Assert.That(controller.SelectFood("嫩叶").Success, Is.True);
+                Assert.That(controller.SelectFood("花朵").Success, Is.True);
+                Assert.That(controller.Points, Is.EqualTo(20));
+            }
+            finally
+            {
+                Object.DestroyImmediate(go);
+            }
+        }
+    }
+}
